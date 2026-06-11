8e1f506de9bc8a94e48d23077a69366fe9cada7a
Giselle Evita Koch <117114437+giselleevita@users.noreply.github.com>
2026-06-11 08:36:42 +0200

    fix: deploy operation-specific Lambda handlers

--- infra/environments/dev/lambda.tf
+++ infra/environments/dev/lambda.tf
@@ -1,16 +1,34 @@
-data "archive_file" "lambda_package" {
+data "archive_file" "upload_presigned_package" {
   type        = "zip"
-  source_file = "${path.root}/../../../app/lambda_function.py"
-  output_path = "${path.root}/../../../app/lambda_function.zip"
+  source_file = "${path.root}/../../../app/lambda_function__upload_presigned.py"
+  output_path = "${path.root}/../../../app/lambda_function__upload_presigned.zip"
+}
+
+data "archive_file" "list_files_package" {
+  type        = "zip"
+  source_file = "${path.root}/../../../app/lambda_function__list_files.py"
+  output_path = "${path.root}/../../../app/lambda_function__list_files.zip"
+}
+
+data "archive_file" "download_file_package" {
+  type        = "zip"
+  source_file = "${path.root}/../../../app/lambda_function__download_file.py"
+  output_path = "${path.root}/../../../app/lambda_function__download_file.zip"
+}
+
+data "archive_file" "delete_file_package" {
+  type        = "zip"
+  source_file = "${path.root}/../../../app/lambda_function__delete_file.py"
+  output_path = "${path.root}/../../../app/lambda_function__delete_file.zip"
 }
 
 resource "aws_lambda_function" "upload_presigned" {
   function_name    = "lambda-upload-presigned"
   runtime          = "python3.12"
-  handler          = "lambda_function.handler"
+  handler          = "lambda_function__upload_presigned.handler"
   role             = module.lambda_roles.upload_role_arn
-  filename         = data.archive_file.lambda_package.output_path
-  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+  filename         = data.archive_file.upload_presigned_package.output_path
+  source_code_hash = data.archive_file.upload_presigned_package.output_base64sha256
 
   environment {
     variables = {
@@ -24,10 +42,10 @@ resource "aws_lambda_function" "upload_presigned" {
 resource "aws_lambda_function" "list_files" {
   function_name    = "lambda-list-files"
   runtime          = "python3.12"
-  handler          = "lambda_function.handler"
+  handler          = "lambda_function__list_files.handler"
   role             = module.lambda_roles.read_role_arn
-  filename         = data.archive_file.lambda_package.output_path
-  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+  filename         = data.archive_file.list_files_package.output_path
+  source_code_hash = data.archive_file.list_files_package.output_base64sha256
 
   environment {
     variables = {
@@ -41,10 +59,10 @@ resource "aws_lambda_function" "list_files" {
 resource "aws_lambda_function" "download_file" {
   function_name    = "lambda-download-file"
   runtime          = "python3.12"
-  handler          = "lambda_function.handler"
+  handler          = "lambda_function__download_file.handler"
   role             = module.lambda_roles.read_role_arn
-  filename         = data.archive_file.lambda_package.output_path
-  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+  filename         = data.archive_file.download_file_package.output_path
+  source_code_hash = data.archive_file.download_file_package.output_base64sha256
 
   environment {
     variables = {
@@ -58,10 +76,10 @@ resource "aws_lambda_function" "download_file" {
 resource "aws_lambda_function" "delete_file" {
   function_name    = "lambda-delete-file"
   runtime          = "python3.12"
-  handler          = "lambda_function.handler"
+  handler          = "lambda_function__delete_file.handler"
   role             = module.lambda_roles.delete_role_arn
-  filename         = data.archive_file.lambda_package.output_path
-  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+  filename         = data.archive_file.delete_file_package.output_path
+  source_code_hash = data.archive_file.delete_file_package.output_base64sha256
 
   environment {
     variables = {
